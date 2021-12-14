fcf0d2849d14dd23e507f30242daba726e12aebf
Yuan He <lawhy0729@gmail.com>
2021-12-14 01:17:15 -0500

    sampling

--- src/ontoplm/onto/onto_text/text_utils.py
+++ src/ontoplm/onto/onto_text/text_utils.py
@@ -23,6 +23,13 @@ import networkx as nx
 from ontoplm.utils import uniqify
 from ontoplm.onto.iris import namespaces, inv_namespaces
 
+##################################################################################
+###                         useful tokenizer paths                             ###
+##################################################################################
+
+BASIC_BERT = "bert-base-uncased"
+BIOCLINICAL_BERT = "emilyalsentzer/Bio_ClinicalBERT"
+
 
 ##################################################################################
 ###                          process entity iris                               ###
@@ -30,7 +37,7 @@ from ontoplm.onto.iris import namespaces, inv_namespaces
 
 
 def abbr_iri(ent_iri: str, sep="#"):
-    """ return the abbreviated iri of an entity given the base iri of its ontology
+    """Return the abbreviated iri of an entity given the base iri of its ontology
     e.g., onto_iri#fragment => onto_prefix:fragment
     """
     if sep == "#" or sep == "/":
@@ -47,7 +54,7 @@ def abbr_iri(ent_iri: str, sep="#"):
 
 
 def unfold_iri(ent_abbr_iri: str):
-    """ unfold the abbreviated iri of an entity given the base iri of its ontology
+    """Unfold the abbreviated iri of an entity given the base iri of its ontology
     e.g., onto_iri#fragment <= onto_prefix:fragment
     """
     base_abbr_iri = ent_abbr_iri.split(":")[0] + ":"
@@ -66,8 +73,8 @@ def unfold_iri(ent_abbr_iri: str):
 def ents_labs_from_props(
     ents: Iterable[EntityClass], ents2idx: Dict, lab_props: List[str] = ["label"]
 ):
-    """ extract unique and cleaned labels (for a group of entities) given the input annotational properties;
-        entities are represented by their numbers according to {ents2idx}
+    """Extract unique and cleaned labels (for a group of entities) given the input annotational properties;
+    entities are represented by their numbers according to {ents2idx}
     """
     ents_labels = defaultdict(list)
     num_labels = 0
@@ -79,14 +86,14 @@ def ents_labs_from_props(
 
 
 def ent_labs_from_props(ent: EntityClass, lab_props: List[str] = ["label"]):
-    """ extract unique and cleaned labels (for an entity) given the input annotational properties
+    """Extract unique and cleaned labels (for an entity) given the input annotational properties
     """
     ent_labels = list(chain(*[prep_labs(ent, lp) for lp in lab_props]))
     return uniqify(ent_labels)
 
 
 def prep_labs(ent: EntityClass, lab_prop: str) -> List[str]:
-    """ preprocess the texts of a class given by a particular property including
+    """Preprocess the texts of a class given by a particular property including
     underscores removal and lower-casing.
 
     Args:
@@ -97,14 +104,38 @@ def prep_labs(ent: EntityClass, lab_prop: str) -> List[str]:
         list: cleaned labels of the input entity
     """
     raw_labels = getattr(ent, lab_prop)
-    assert type(raw_labels) is IndividualValueList
+    assert isinstance(raw_labels, IndividualValueList)
     cleaned_labels = [lab.lower().replace("_", " ") for lab in raw_labels]
     return cleaned_labels
 
 
 ##################################################################################
-###                         useful tokenizer paths                             ###
+###                 naive disjointness of sibling classes                      ###
 ##################################################################################
 
-BASIC_BERT = "bert-base-uncased"
-BIOCLINICAL_BERT = "emilyalsentzer/Bio_ClinicalBERT"
+
+def sibling_labels(
+    ents: Iterable[EntityClass], lab_props: List[str] = ["label"]
+) -> List[Tuple[Tuple[str]]]:
+    """Return all the sibling label groups with size > 1 and no duplicates
+    """
+    sib_lables = []
+    for ent in ents:
+        ch_labels = child_labels(ent, lab_props)
+        if ch_labels and len(ch_labels) > 1:
+            sib_lables.append(ch_labels)
+    sib_lables = uniqify(sib_lables)
+    # change type to 3-d lists
+    return list(map(lambda dis_group: [list(syn_group) for syn_group in dis_group], sib_lables))
+
+
+def child_labels(ent: EntityClass, lab_props: List[str] = ["label"]) -> Tuple[Tuple[str]]:
+    """Return labels of child entity classes, ensuring that not label groups are duplicated 
+    """
+    return tuple(
+        set(
+            tuple(ent_labs_from_props(child, lab_props))
+            for child in ent.subclasses()
+            if isinstance(child, EntityClass)
+        )
+    )
