3336a220a61bebc8582802bd89aab1c53613e6b8
Lawhy <yuan.he.oxcs@gmail.com>
2021-12-15 23:48:08 -0500

    rename

--- src/deeponto/onto/onto_text/text_utils.py
+++ src/deeponto/onto/onto_text/text_utils.py
@@ -14,14 +14,14 @@
 
 from owlready2.entity import EntityClass
 from owlready2.prop import IndividualValueList
-from typing import Iterable, List, Dict, Tuple, Set
+from typing import Iterable, List, Dict, Tuple
 
 from collections import defaultdict
 from itertools import chain
 import networkx as nx
 
-from ontoplm.utils import uniqify
-from ontoplm.onto.iris import namespaces, inv_namespaces
+from deeponto.utils import uniqify
+from deeponto.onto.iris import namespaces, inv_namespaces
 
 ##################################################################################
 ###                         useful tokenizer paths                             ###
