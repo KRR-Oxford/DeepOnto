ee3557aea4a4d0df7ff85ec4436eb3bb9a9afca6
Yuan He <lawhy0729@gmail.com>
2022-01-19 13:20:01 +0000

    add negative sampler

--- src/deeponto/data/align/neg_cands.py
+++ src/deeponto/data/align/neg_cands.py
@@ -13,3 +13,101 @@
 # limitations under the License.
 """Provide functions for generating negative candidates"""
 
+from itertools import cycle
+from pyats.datastructures import AttrDict
+import random
+
+from deeponto.onto import Ontology
+from deeponto.onto.text import Tokenizer
+from deeponto.onto.mapping import OntoMappings, EntityMapping
+
+
+# TODO: to be updated constantly
+sampling_options = ["idf"]
+
+
+class OntoAlignNegCandsSampler:
+    def __init__(
+        self, src_onto: Ontology, tgt_onto: Ontology, tsv_mappings_path: str, tokenizer: Tokenizer
+    ):
+        self.src_onto = src_onto
+        self.tgt_onto = tgt_onto
+        # loaded mappings are served as +ve candiates (anchors)
+        pos_ent_pairs = OntoMappings.read_tsv_mappings(tsv_mappings_path)
+        self.pos_src2tgt = dict(pos_ent_pairs)
+        self.pos_tgt2src = dict([(y, x) for (x, y) in pos_ent_pairs])
+        self.tokenizer = tokenizer
+
+        self.flag_set = cycle(["src2tgt", "tgt2src"])
+        self.flag = next(self.flag_set)
+
+    def sample(self, n_cands: int, strategy: str):
+        """Sample negative candidates using provided strategy for both sides (src and tgt)
+        """
+        if strategy in sampling_options:
+            # implemented sample names are all in: "xx_sample"
+            sampler = getattr(self, f"{strategy}_sample")
+            self.renew()
+            fixed_src_ent_pairs = sampler(n_cands)
+            self.switch()
+            fixed_tgt_ent_pairs = sampler(n_cands)
+            return AttrDict({"fixed_src": fixed_src_ent_pairs, "fixed_tgt": fixed_tgt_ent_pairs})
+        else:
+            raise ValueError(f"Valid sampling options are: {sampling_options}")
+
+    def renew(self):
+        """Renew alignment direction to src2tgt
+        """
+        while self.flag != "src2tgt":
+            self.switch()
+
+    def switch(self):
+        """Switch alignment direction
+        """
+        self.src_onto, self.tgt_onto = self.tgt_onto, self.src_onto
+        self.flag = next(self.flag_set)
+
+    def random_select(self, n_cands: int):
+        """Randomly select n target candidates
+        """
+        return random.sample(range(len(self.tgt_onto.class2idx)), n_cands)
+
+    ##################################################################################
+    ###                            sampling stratgies                              ###
+    ##################################################################################
+
+    def idf_sample(self, n_cands: int) -> OntoMappings:
+        """Sample negative candidates using inverted index-based idf scoring
+        """
+
+        # rel "?" means to-be-confirmed
+        ent_pairs = OntoMappings(flag=self.flag, n_best=n_cands, rel="?")
+
+        for src_ent, tgt_ent in getattr(self, f"pos_{self.flag}").items():
+
+            # select n candidates for the src ent
+            src_ent_id = self.src_onto.class2idx[src_ent]
+            src_ent_labs = self.src_onto.idx2labs[src_ent_id]
+            src_ent_toks = self.tokenizer.tokenize_all(src_ent_labs)
+            tgt_cand_ids = self.tgt_onto.idf_select(src_ent_toks, n_cands)  # [(ent_id, idf_score)]
+
+            if not tgt_cand_ids:
+                # randomly select when idf select nothing
+                print(f"idf select nothing => randomly select {n_cands} candidates ...")
+                tgt_cand_ids = self.random_select(n_cands)
+            else:
+                # unzip to list of ids
+                tgt_cand_ids = list(list(zip(*tgt_cand_ids))[0])
+            # check if the positive pair is included
+            tgt_ent_id = self.tgt_onto.class2idx[tgt_ent]
+            if not tgt_ent_id in tgt_cand_ids:
+                tgt_cand_ids.insert(0, tgt_ent_id)
+
+            # store the mapping
+            for tgt_cand_id in tgt_cand_ids[:n_cands]:
+                tgt_cand = self.tgt_onto.idx2class[tgt_cand_id]
+                score = 1.0 if tgt_cand_id == tgt_ent_id else 0.0
+                tbc_mapping = EntityMapping(src_ent, tgt_cand, rel="?", score=score)
+                ent_pairs.add(tbc_mapping)
+
+        return ent_pairs
