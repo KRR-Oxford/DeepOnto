9ad40b5890e8a078fd121b5191c13207d0a7ad07
Yuan He <lawhy0729@gmail.com>
2022-04-20 05:29:43 +0100

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A package for ontology engineering with pre-trained language models.',
+    description='DeepOnto aims to provide implemented deep learning models and an evaluation platform for various ontology engineering purposes. ',
     author='Yuan He (KRR-Oxford)',
     license='',
 )
