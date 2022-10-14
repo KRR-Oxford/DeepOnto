058882cdd453324a4129f26dd5317e748438c722
Lawhy <lawhy@gmail.com>
2022-10-15 00:21:31 +0100

    update probe

--- src/deeponto/data/inference/__init__.py
+++ src/deeponto/data/inference/__init__.py
@@ -11,23 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from datasets import load_dataset
-from openprompt.data_utils import InputExample
-from .subs_pairs import SubsumptionPairGenerator
-
-
-def load_prompt_data(dataset_path: str = "multi_nli", *splits: str):
-    """Load a datast from huggingface datasets and transform to openprompt examples.
-    """
-    data_dict = load_dataset(dataset_path)
-    prompt_data_dict = dict()
-    splits = list(splits) if splits else data_dict.keys()
-    i = 0
-    for split in splits:
-        x_samples = []
-        for samp in data_dict[split]:
-            inp = InputExample(guid=i, meta=samp, label=samp["label"])
-            x_samples.append(inp)
-            i += 1
-        prompt_data_dict[split] = x_samples
-    return prompt_data_dict
+from . import inference_utils
