09f9f20ddffd92d3ddab2426a65a9bc493c13b6f
Lawhy <lawhy@gmail.com>
2022-11-12 04:10:53 +0000

    complete reasoner

--- src/deeponto/data/infer/subs/atomic_subs.py
+++ src/deeponto/data/infer/subs/atomic_subs.py
@@ -18,53 +18,22 @@ import random
 from typing import Callable, Optional, List
 import enlighten
 from collections import defaultdict
+from yacs.config import CfgNode
 
 from deeponto.onto import Ontology
 from deeponto.onto.logic.reasoner import OWLReasoner
 from deeponto import SavedObj, OWL_THING
+from . import SubsumptionSamplerBase
 
 
-class AtomicSubsumptionSampler:
+class AtomicSubsumptionSampler(SubsumptionSamplerBase):
     def __init__(
-        self,
-        onto_path: str,
-        lab_props: List[str] = ["http://www.w3.org/2000/01/rdf-schema#label"],
-        neg_ratio: int = 1,
-        hard_neg_ratio: Optional[int] = None,
+        self, onto: Ontology, neg_ratio: int = 1, hard_neg_ratio: Optional[int] = None,
     ):
-        self.reasoner = OWLReasoner(onto_path)
-        self.onto = Ontology.from_new(onto_path, lab_props)
-        self.class_iris = list(self.reasoner.owlClasses.keys())
-        self.class_iris_with_root = self.class_iris + [OWL_THING]
-        self.obj_prop_iris = list(self.reasoner.owlObjectProperties.keys())
-        self.subs = self.init_subs()
-        self.neg_ratio = neg_ratio
+        super().__init__(onto, neg_ratio)
         # set the hard negative ratio the same as the soft negative ratio if not specified
         self.hard_neg_ratio = hard_neg_ratio if hard_neg_ratio else neg_ratio
-        # pre-compute classes that have multiple (inferred and direct) children
-        self.non_single_child_classes = dict()
-        self.sibling_pairs = []
-        for cl_iri in self.class_iris_with_root:
-            owlClass = self.reasoner.owlClass_from_iri(cl_iri)
-            children = self.reasoner.subclasses_of(owlClass, direct=True)
-            if len(children) >= 2:
-                self.non_single_child_classes[cl_iri] = children
-                self.sibling_pairs += [
-                    (x, y) for x, y in itertools.product(children, children) if x != y
-                ]  # all possible combinations excluding reflexive pairs
-        self.sibling_pairs = list(set(self.sibling_pairs))
-        # an additional sibling dictionary for customized (fixed one sample) sampling
-        self.sibling_dict = defaultdict(list)
-        for l, r in self.sibling_pairs:
-            self.sibling_dict[l].append(r)
-            self.sibling_dict[r].append(l)
-        print(
-            f"{len(self.non_single_child_classes)}/{len(self.class_iris_with_root)} (including Owl:Thing) has multiple (direct and inferred) children ..."
-        )
-        print(
-            f"In total there are {len(self.sibling_pairs)} sibling class pairs (order matters) ..."
-        )
-
+        
     def init_subs(self):
         return {
             "positive": [],
@@ -76,32 +45,26 @@ class AtomicSubsumptionSampler:
         self.subs = self.init_subs()
         self.subs["positive"] = self.entailment_pairs()
         self.subs["soft_negative"] = self.contradiction_pairs(
-            self.soft_neg_sample, max_neg_num=int(self.neg_ratio * len(self.subs["positive"]))
+            self.random_atomic_class_pair, max_neg_num=int(self.neg_ratio * len(self.subs["positive"]))
         )
         hard_neg_num = min(
-            self.hard_neg_ratio * len(self.subs["positive"]), len(self.sibling_pairs)
+            self.hard_neg_ratio * len(self.subs["positive"]), len(self.reasoner.sibling_pairs)
         )
         self.subs["hard_negative"] = self.contradiction_pairs(
-            self.hard_neg_sample, max_neg_num=int(hard_neg_num)
+            self.random_sibling_atomic_class_pair, max_neg_num=int(hard_neg_num)
         )
         assert not set(self.subs["positive"]).intersection(self.subs["soft_negative"])
         assert not set(self.subs["positive"]).intersection(self.subs["hard_negative"])
 
-    def save(self, saved_path: str):
-        """Save in json format similar to the huggingface datasets manner
-        """
-        SavedObj.save_json(self.subs, saved_path)
-
     def entailment_pairs(self):
-        """Extract all subsumption pairs that indicate entailment from left to right,
+        """Extract all atomic subsumption pairs that indicate entailment from left to right,
         i.e., if A is {left} => then A is {right}.
         """
-        manager = enlighten.get_manager()
-        pbar = manager.counter(desc="Extract Subsumption:", unit="pair")
+        pbar = self.progress_manager.counter(desc="Extract Subsumption:", unit="pair")
         positives = []
-        for cl_iri in self.class_iris:
-            owl_cl = self.reasoner.owlClass_from_iri(cl_iri)
-            for subsumer_iri in self.reasoner.superclasses_of(owl_cl):
+        for cl_iri in self.reasoner.class_iris:
+            owl_cl = self.reasoner.owlClasses[cl_iri]
+            for subsumer_iri in self.reasoner.supers_of(owl_cl):
                 positives.append(f"{cl_iri} <SubsumedBy> {subsumer_iri}")
                 pbar.update()
         positives = list(set(sorted(positives)))
@@ -109,21 +72,18 @@ class AtomicSubsumptionSampler:
         return positives
 
     def contradiction_pairs(self, sample_a_pair: Callable, max_neg_num: int):
-        """Sample false subsumption pairs that indicate contradiction from left to right,
-        i.e., if A is {left} !=> then A is {right}. Such pairs meet one of the two criteria:
-            1. they are disjoint (after reasoning);
-            2. they do not have a common descendant (including themselves, i.e., they do 
-            not have a inferred subsumption relationship).
+        """Sample false atomic subsumption pairs that indicate contradiction from left to right,
+        i.e., if A is {left} !=> then A is {right}. Such pairs need to pass the negative sample
+        check.
         """
         negatives = []
         max_iter = 2 * max_neg_num
         print(f"Sample false subsumption pairs with method: {sample_a_pair.__name__}.")
         # create two bars for process tracking
-        manager = enlighten.get_manager()
-        added_bar = manager.counter(
+        added_bar = self.progress_manager.counter(
             total=max_neg_num, desc="Sampling False Subsumption", unit="pair"
         )
-        iter_bar = manager.counter(total=max_iter, desc="#Iteration", unit="it")
+        iter_bar = self.progress_manager.counter(total=max_iter, desc="#Iteration", unit="it")
         i = 0
         added = 0
         while added < max_neg_num and i < max_iter:
@@ -133,8 +93,10 @@ class AtomicSubsumptionSampler:
                 i += 1
                 iter_bar.update(1)
                 continue
-            # collect class label if accepted
-            if self.sanity_check(left_class_iri, right_class_iri):
+            left_class = self.reasoner.owlClasses[left_class_iri]
+            right_class = self.reasoner.owlClasses[right_class_iri]
+            # collect class iri if accepted
+            if self.reasoner.check_negative_subsumption(left_class, right_class):
                 neg = f"{left_class_iri} <NotSubsumedBy> {right_class_iri}"
                 negatives.append(neg)
                 added += 1
@@ -148,66 +110,6 @@ class AtomicSubsumptionSampler:
         negatives = list(set(sorted(negatives)))
         print(f"In total {len(negatives)} unique false subsumption pairs are extracted.")
         return negatives
-
-    def soft_neg_sample(self):
-        """[Auxiliary]: soft negatives are randomly sampled
-        """
-        return tuple(random.sample(self.class_iris, k=2))
-
-    def hard_neg_sample(self):
-        """[Auxiliary]: hard negatives are sampled from sibling classes (direct children of a parent class)
-        """
-        return tuple(random.choice(self.sibling_pairs))
-
-    def sanity_check(self, left_class_iri: str, right_class_iri: str):
-        """[Auxiliary]: sanity check for a given negative sample
-        """
-        accepted = False
-        # get the owlapi class objects
-        owl_left = self.reasoner.owlClass_from_iri(left_class_iri)
-        owl_right = self.reasoner.owlClass_from_iri(right_class_iri)
-        # NOTE: Test 1: check for disjointness (after reasoning)
-        if self.reasoner.check_disjoint(owl_left, owl_right):
-            accepted = True
-        else:
-            # NOTE: Test 2: check any common descendants including themselves (after reasoning)
-            left_descendant_iris = self.reasoner.subclasses_of(owl_left)
-            left_descendant_iris.append(left_class_iri)
-            right_descendant_iris = self.reasoner.subclasses_of(owl_right)
-            right_descendant_iris.append(right_class_iri)
-            if not set(left_descendant_iris).intersection(set(right_descendant_iris)):
-                accepted = True
-        return accepted
-
-    def neg_sample_for_class(self, class_iri: str, try_hard: bool = True):
-        """Generate a customized negative sample (atomic classes) with one side fixed
-        """
-        if try_hard:
-            try:
-                neg = random.choice(self.sibling_dict[class_iri])
-                if self.sanity_check(neg):
-                    return neg
-            except:
-                pass
-        # if failed to try hard sample
-        neg = random.choice(self.class_iris)
-        if self.sanity_check(class_iri, neg):
-            return neg
-        else:
-            return None
-
-    def neg_sample_for_object_property(self, property_iri: str):
-        """Generate a customized negative sample (object property) with one side fixed
-        """
-        prop = self.reasoner.owlObjectProperties[property_iri]
-        prop_subs = self.reasoner.sub_object_properties_of(prop) + [property_iri]
-        neg_iri = random.choice(self.obj_prop_iris)
-        neg = self.reasoner.owlObjectProperties[neg_iri]
-        neg_subs = self.reasoner.sub_object_properties_of(neg) + [neg_iri]
-        if not set(prop_subs).intersection(set(neg_subs)):
-            return str(neg.getIRI())
-        else:
-            return None
 
     def random_select_lab_pair(self, left_class_iri: str, right_class_iri: str):
         """[Auxiliary]: randomly select a pair of labels from two classes
