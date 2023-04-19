6b650a690eadbcb43038424d7303ee779e8660f4
Lawhy <yuan.he.oxcs@gmail.com>
2023-04-19 13:03:35 +0100

    update license info

--- src/deeponto/align/bertsubs/__init__.py
+++ src/deeponto/align/bertsubs/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Yuan He (KRR-Oxford). All rights reserved.
+# Copyright 2021 Yuan He. All rights reserved.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
