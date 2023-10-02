eb0f2ce602e963315226c9b243188e5352449a63
Lawhy <yuanhe.cs.ai@gmail.com>
2023-10-02 18:29:41 +0100

    update

--- src/deeponto/onto/__init__.py
+++ src/deeponto/onto/__init__.py
@@ -16,4 +16,4 @@ from .pruning import OntologyPruner
 from .verbalisation import OntologyVerbaliser, OntologySyntaxParser
 from .projection import OntologyProjector
 from .normalisation import OntologyNormaliser
-from .taxonomy import OntologyTaxonomy, WordnetTaxonomy
+from .taxonomy import Taxonomy, OntologyTaxonomy, WordnetTaxonomy, TaxonomyNegativeSampler
